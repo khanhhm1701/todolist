6e3b8202ee1e779eacaa306824c21a7f282789da
Hoang Minh Khanh <96485127+khanhhm1701@users.noreply.github.com>
2023-03-17 12:01:58 +0700

    fix UI

--- src/parts/ContentJobDone.jsx
+++ src/parts/ContentJobDone.jsx
@@ -1,12 +1,36 @@
-import "../assets/css/TodoList.css";
-export default function ContentJobDone({selectedJob}) {
+import "../assets/css/ContentJobDone.css";
+import done from "../assets/img/done.png";
+import firework from "../assets/img/firework.gif"; //
+export default function ContentJobDone({ selectedJob }) {
     return (
-        <div className="selectedContent">
-            <h2 className="selected-title">{selectedJob.title}</h2>
-            <p className="in-date">Innitiated Date: {selectedJob.inDate}</p>
-            <p className="due-date">Due Date: {selectedJob.dueDate}</p>
-            <p className="selected-detail">{selectedJob.content}</p>
-            {selectedJob.isChecked && <h2 className="done-mess">This job is done!!!</h2>}
+        <div className="wrapper">
+            {selectedJob.isChecked ? (
+                <div className="selected-content checked">
+                    <h2 className="selected-title">{selectedJob.title}</h2>
+                    <p className="in-date"><strong>Innitiated Date:</strong> {selectedJob.inDate}</p>
+                    <p className="due-date"><strong>Due Date:</strong> {selectedJob.dueDate}</p>
+                    <p className="selected-detail"><strong>Detail Content: </strong>{selectedJob.content}</p>
+                    <div className="done-show">
+                        <img src={done} alt="" className="done-img" />
+                        <div className="done-content">
+                            <img src={firework} alt="" className="firework firework1" />
+                            <div className="done-title">
+                                <span>This job is done!!!</span>
+                            </div>
+                            <img src={firework} alt="" className="firework firework2" />
+                        </div>
+                    </div>
+
+                </div>
+            ) : (
+                <div className="selected-content unchecked">
+                    <h2 className="selected-title">{selectedJob.title}</h2>
+                    <p className="in-date"><strong>Innitiated Date:</strong> {selectedJob.inDate}</p>
+                    <p className="due-date"><strong>Due Date:</strong> {selectedJob.dueDate}</p>
+                    <p className="selected-detail"><strong>Detail Content: </strong>{selectedJob.content}</p>
+                </div>
+            )
+            }
         </div>
     )
 }
